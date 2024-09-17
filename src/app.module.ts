f6cd14f9d8790267bb0b651dea18d54a0819c314
AllanGCalixto <allancalixto03@gmail.com>
2024-09-16 21:50:43 -0300

    conectando projeto ao banc

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MongooseModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: async(configService: ConfigService)=> ({
+      uri: configService.get<string>('MONGO_STRING'),
+    }),
+
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
